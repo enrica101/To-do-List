e00c95f924fbda0c58105e39eb4692987b32349c
ecarpena <enricajulescarpena@gmail.com>
2021-02-22 20:12:40 +0800

    Enter key pressed on add
    When pressed enter, new item will be added on the list.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 const textInput = document.querySelector(".input");
 const addBtn = document.querySelector(".btn-add");
-const todoContainer = document.querySelector('.to-do')
+const todoContainer = document.querySelector(".to-do");
 const list = document.querySelector(".list");
-const dropdowns = document.querySelectorAll(".dropdown");
-let listItems;
+const editBtn = document.querySelector(".edit");
+const removeBtn = document.querySelector(".remove");
+const dropdownItems = document.querySelectorAll('.dropdown-item')
 
+let listItems;
+//adding items to list on click
 addBtn.addEventListener("click", addItem);
+
+//enter key pressed listener
+textInput.addEventListener('keyup', (event) => {
+  if(event.keyCode === 13) {
+    addItem()
+  }
+})
 
 function addItem() {
   const text = textInput.value;
@@ -14,7 +24,7 @@ function addItem() {
   item.classList.add("list-item");
 
   listItems = document.querySelectorAll(".list-item");
-  let items = listItems.length + 1
+  let items = listItems.length + 1;
   if (text !== "") {
     item.innerHTML = `
      <span>
@@ -24,14 +34,20 @@ function addItem() {
             <div class="dropdown">
               <button class="btn btn-dropdown dropdown-toggle" data-toggle="dropdown"></button>
               <div class="dropdown-menu">
-                <a class="dropdown-item" href="#"><button id="edit">Edit</button></a>
-                <a class="dropdown-item" href="#"><button id="remove">Remove</button></a>
+                <a class="dropdown-item edit" href="#"><button id="edit">Edit</button></a>
+                <a class="dropdown-item remove" href="#"><button id="remove">Remove</button></a>
               </div>
             </div>`;
     list.appendChild(item);
     textInput.value = "";
+    console.log(list.childNodes);
+
+  if(container.offsetHeight > 600){
+    container.classList.add('extend')
+  }
   } else {
     alert("Field is empty.");
   }
+
 
 }
